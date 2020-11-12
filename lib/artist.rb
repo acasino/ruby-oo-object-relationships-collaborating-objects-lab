219a8b0edcf4d06e4236c4b042d195c9bd3de68a
Andrew <andrew.casin@gmail.com>
2020-11-11 23:30:21 -0600

    commit tree 1684c8ff833db6e709ed2544340927f552b80e48

---
+++ lib/artist.rb
@@ -0,0 +1,44 @@
+class Artist
+    attr_accessor :name
+
+    @@all = []
+
+    def initialize(name)
+        @name = name
+        save
+    end
+
+    def save
+        @@all << self
+    end
+
+    def self.all
+        @@all
+    end
+
+    def add_song(song)
+        song.artist = self
+    end
+
+    def songs
+        Song.all.select {|song| song.artist == self}
+    end
+
+    def self.find_or_create_by_name(name)
+        ##find artist instance that has that name or create if none exists. return will be instance of artist w/name attribute filled out
+        self.find(name) ? self.find(name) : self.new(name)
+    end
+
+    def self.find(name)
+        self.all.find {|artist| artist.name == name}
+    end
+
+    def print_songs
+        ##output names of all songs associated with this Artist instance
+        songs.each {|song| puts song.name}
+    end
+
+
+
+
+end
